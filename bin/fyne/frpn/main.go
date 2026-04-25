e8aaa0901de8f485af5cbfa701191b18943bfc0c
mattwach <matt.wachowski@gmail.com>
2026-04-25 15:54:56 -0700

    Initial readline integration

--- bin/fyne/frpn/main.go
+++ bin/fyne/frpn/main.go
@@ -4,21 +4,45 @@ package main
 import (
 	"fmt"
 	"mattwach/rpngo/common/functions"
+	"mattwach/rpngo/common/parse"
 	"mattwach/rpngo/common/rpn"
 	"os"
+
+	"github.com/chzyer/readline"
 )
+
+func execLine(r *rpn.RPN, rl *readline.Instance) error {
+	line, err := rl.Readline()
+	if err != nil {
+		return err
+	}
+	err = parse.Fields(line, r.Exec)
+	if err != nil {
+		fmt.Printf("error: %v\n", err)
+	}
+
+	for _, f := range r.Frames {
+		fmt.Println(f.String(true))
+	}
+
+	return nil
+}
 
 func run() error {
 	var r rpn.RPN
 	r.Init(256)
 	functions.RegisterAll(&r)
 
-	if err := r.ExecSlice(os.Args[1:]); err != nil {
-		return err
+	rl, err := readline.New("> ")
+	if err != nil {
+		panic(err)
 	}
+	defer rl.Close()
 
-	for _, f := range r.Frames {
-		fmt.Println(f.String(true))
+	for {
+		if err := execLine(&r, rl); err != nil {
+			break
+		}
 	}
 
 	return nil
